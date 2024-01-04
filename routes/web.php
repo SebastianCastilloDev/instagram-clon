1cace53182e0e8f942bb2b764f21706145016472
Sebastian Castillo <tantrum5535@gmail.com>
2024-01-04 18:54:08 -0300

    creando el template de registro

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('principal');
 });
+Route::get('/crear-cuenta', function () {
+    return view('auth.register');
+});
 
