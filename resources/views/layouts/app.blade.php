908a5fa758b835f8d0878ad27783d5beb891661b
Sebastian Castillo <tantrum5535@gmail.com>
2024-01-06 12:55:17 -0300

    cerrando la sesion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
                     <a class="font-bold text-gray-600 text-sm" href="#">
                         Hola: <span class="font-normal">{{ auth()->user()->username }}</span>
                     </a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Cerrar Sesión</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">Cerrar Sesión</a>
                 </nav>
             @endauth
 
