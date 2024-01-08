4ba1c8455ade9ec5a483cf905152e627d07016bb
Sebastian Castillo <tantrum5535@gmail.com>
2024-01-08 15:58:34 -0300

    Paginando las publicaciones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
 
-        $@if ($posts->count())
+        @if ($posts->count())
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach ($posts as $post)
                     <div>
@@ -41,6 +41,9 @@
                         </a>
                     </div>
                 @endforeach
+            </div>
+            <div>
+                {{ $posts->links('pagination::tailwind') }}
             </div>
         @else
             <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
