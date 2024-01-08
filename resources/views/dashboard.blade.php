2c71931afe653098b07c0acd6d201328e4830f83
Sebastian Castillo <tantrum5535@gmail.com>
2024-01-08 16:13:49 -0300

    estilos de tailwindcss a la paginacion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,8 +42,8 @@
                     </div>
                 @endforeach
             </div>
-            <div>
-                {{ $posts->links('pagination::tailwind') }}
+            <div class="my-10">
+                {{ $posts->links() }}
             </div>
         @else
             <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
