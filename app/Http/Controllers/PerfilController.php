6f66477c1c6f4fe6f79360f829e769c09d831bcd
Sebastian Castillo <tantrum5535@gmail.com>
2024-01-09 11:45:25 -0300

    almacenar la informacion del usuario

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 class PerfilController extends Controller
 {
@@ -25,5 +28,28 @@ class PerfilController extends Controller
         $this->validate($request, [
             'username'=>'required|unique:users,username|min:3|max:20|not_in:twitter,editar-perfil'
         ]);
+
+        if($request->imagen){
+            $imagen = $request->file('imagen');
+ 
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+    
+            $manager = new ImageManager(new Driver());
+            $imagenServidor = $manager::imagick()->read($imagen);
+            $imagenServidor->resizeDown(1000, 1000);
+    
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            
+            $imagenServidor->save($imagenPath);
+        }
+
+        //Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+
+        //Redireccionar
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
